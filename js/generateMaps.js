b12280ec12fcf44cc8913fc9be797e61bcc18bf0
Victor Evogor <victorevogor0001@gmail.com>
2024-01-02 04:52:15 +0100

    fix castello default checked radio buttons

--- js/generateMaps.js
+++ js/generateMaps.js
@@ -1,8 +1,8 @@
 function generateMapHTML(map) {
     return `
       <div class="layer-list-row">
-        <input class="${map.id}" type="radio" name="ltoggle" value="${map.id}" ${map.checked ? 'checked="checked"' : ''} />
-        <input class="${map.id}" type="radio" name="rtoggle" value="${map.id}" />
+        <input class="${map.id}" type="radio" name="ltoggle" value="${map.id}" />
+        <input class="${map.id}" type="radio" name="rtoggle" value="${map.id}" ${map.checked ? 'checked="checked"' : ''}/>
         &nbsp;
         <label for="${map.id}">${map.name}<div class="dummy-label-layer-space"></div></label>
         <div class="layer-buttons-block">
