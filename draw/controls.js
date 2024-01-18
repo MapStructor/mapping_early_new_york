7416193ece6aafe9fbfe782a7eff0271c0a92ed9
Victor Evogor <victorevogor0001@gmail.com>
2024-01-18 22:23:18 +0100

    added labels

--- draw/controls.js
+++ draw/controls.js
@@ -93,7 +93,54 @@ function onSelectFeature(e, mapType) {
     document.getElementById(title).value =
       feature.properties.title || "";
     document.getElementById(info).value = feature.properties.info || "";
+    console.log(feature.geometry.coordinates)
+    let coordinates;
+
+    if (feature.geometry.coordinates.length === 2){
+      coordinates = feature.geometry.coordinates;
+    } else
+     coordinates = feature.geometry.coordinates[0][0];
+    console.log(coordinates)
+  var label = {
+    id: feature.properties.title,
+    type: "symbol",
+    source: {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: {
+              title: feature.properties.title || "No title set",
+              icon: "circle",
+            },
+            geometry: {
+              type: "Point",
+              coordinates: coordinates,
+            },
+          },
+        ],
+      },
+    },
+    layout: {
+      visibility: "visible",
+      "text-font": ["Asap Medium"],
+      "text-field": "{title}",
+      "text-size": 18,
+    },
+    paint: {
+      "text-color": "#000000",
+      "text-halo-width": 3,
+      "text-halo-blur": 2,
+      "text-halo-color": "#ffffff",
+    },
+  };
+
+  beforeMap.addLayer(label)
   }
+
+  
 }
 
 function updateFeatureInfo(mapType) {
